7597b5b284d6ea128f653c75e0c6a6b67ee43d34
Sahil Mehra <sahilpmehra@yahoo.co.in>
2019-12-17 09:17:44 +0530

    testing

--- data.py
+++ data.py
@@ -134,8 +134,7 @@ def randomRotate90(image, mask, u=0.5):
 def default_loader(id, root):
     img = cv2.imread(os.path.join(root,'{}_sat.jpg').format(id))
     mask = cv2.imread(os.path.join(root+'{}_mask.png').format(id), cv2.IMREAD_GRAYSCALE)
-    
-    img, mask = reScale(img, mask, 256)    
+        
     img = randomHueSaturationValue(img,
                                    hue_shift_limit=(-30, 30),
                                    sat_shift_limit=(-5, 5),
@@ -149,6 +148,8 @@ def default_loader(id, root):
     img, mask = randomHorizontalFlip(img, mask)
     img, mask = randomVerticleFlip(img, mask)
     img, mask = randomRotate90(img, mask)
+    
+    img, mask = reScale(img, mask, 256)
     
     
     mask = np.expand_dims(mask, axis=2)
